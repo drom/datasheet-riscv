3b186c8b9edbb523e2c28b4801570b107c7e040f
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2019-03-08 23:03:00 -0800

    more style cleanup

--- bin/build.js
+++ bin/build.js
@@ -4,64 +4,34 @@ const util = require('util');
 const stream = require('stream');
 const fs = require('fs-extra');
 const asciidoctor = require('asciidoctor.js');
-const template = require('../lib/template.js')
 const browserify = require('browserify');
+
+const template = require('../lib/template.js')
+const style = require('../lib/style.js')
 
 const a = asciidoctor();
 const b = browserify();
 
-const fonts = `
-<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+const fonts = [
+  'Open+Sans',
+  'IBM+Plex+Sans',
+  'Roboto+Mono'
+]
+  .map(e => '<link href="https://fonts.googleapis.com/css?family=' + e + '" rel="stylesheet">')
+  .join('\n');
+
+const getFront = doc => `
+<div class="ftitle">
+<div class="ftitle1">${doc.getTitle()}</div>
+<div class="ftitle2">${doc.getAuthor()}</div>
+</div>
 `;
-
-const style = `
-body {
-  font-size: 16px;
-  font-family: 'Open Sans', sans-serif;
-}
-.note {
-  font-size: 0.8em;
-}
-
-.icon {
-  padding: 5px;
-  color: #fff;
-  border-right: 1px solid #aaa;
-}
-#toctitle {
-  display: block;
-  font-size: 1.5em;
-  font-weight: bold;
-}
-
-@media print {
-  a {
-    text-decoration-line: none;
-    color: #000;
-  }
-  body {
-    font-size: 20px;
-  }
-  @page {
-    margin: 25mm;
-  }
-  h2 {
-    page-break-before: always;
-  }
-}
-`;
-// `
-// #spacer {
-//   shape-outside: polygon(0 0, 800px 0, 800px 50px, 0 50px, 0 700px, 800px 700px, 800px 750px, 0 750px, 0 1500px, 800px 1500px, 800px 1550px, 0 1550px);
-//   width: 800px;
-//   height: 3200px;
-//   float: left;
-// }
-// `;
 
 const main = async () => {
   const abody = await fs.readFile('./riscv-v-spec/v-spec.adoc', 'utf8');
-  const body = a.convert(abody);
+  const dbody = a.load(abody);
+  // console.log(dbody.getTitle(), dbody.getAuthor());
+  const body = dbody.convert();
   b.add('./src/app.js');
   b.bundle(function (err, script) {
     if (err) {
@@ -71,6 +41,7 @@ const main = async () => {
       title: 'Vector',
       fonts: fonts,
       style: style,
+      front: getFront(dbody),
       body: body,
       script: script
     });
--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
 'use strict';
 
+// const datasheet = require('datasheet');
+
+window.matchMedia('print').addListener(e => {
+  console.log('mql');
+  console.log(e);
+});
+
 console.log('app is up');
