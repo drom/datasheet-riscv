d7f90d5ff6c4c6c423e6cbf3f2026ea5d61fb638
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2019-03-19 17:27:27 -0700

    unified bundler for webapp and nwapp

--- bin/build-nwapp.js
+++ bin/build-nwapp.js
@@ -1,55 +1,6 @@
 #!/usr/bin/env node
 'use strict';
 
-const fs = require('fs-extra');
-const asciidoctor = require('asciidoctor.js');
-const browserify = require('browserify');
+const bundler = require('../lib/bundler.js');
 
-const template = require('../lib/template.js');
-const style = require('../lib/style.js');
-
-const a = asciidoctor();
-const b = browserify();
-
-const fonts = [
-  'Open+Sans',
-  'IBM+Plex+Sans',
-  'Roboto+Mono'
-]
-  .map(e => '<link href="https://fonts.googleapis.com/css?family=' + e + '" rel="stylesheet">')
-  .join('\n');
-
-const getFront = doc => `
-<div class="ftitle">
-<div class="ftitle1">${doc.getTitle()}</div>
-<div class="ftitle2">${doc.getAuthor()}</div>
-</div>
-`;
-
-const main = async () => {
-  // const abody = await fs.readFile('./riscv-v-spec/v-spec.adoc', 'utf8');
-
-  const highlightcss = await fs.readFile('./node_modules/highlight.js/styles/default.css', 'utf8');
-
-  const dbody = a.loadFile('./riscv-v-spec/v-spec.adoc');
-  // dbody.pushInclude('./riscv-v-spec/vmem-format.adoc');
-  // console.log(dbody.getTitle(), dbody.getAuthor());
-  const body = dbody.convert();
-  b.add('./lib/nwapp.js');
-  b.bundle(function (err, script) {
-    if (err) {
-      throw new Error(err);
-    }
-    const doc = template({
-      title: 'Vector',
-      fonts: fonts,
-      style: highlightcss + style,
-      front: getFront(dbody),
-      body: body,
-      script: script
-    });
-    fs.outputFile('./nwapp/index.html', doc);
-  });
-};
-
-main();
+bundler('./lib/nwapp.js', './nwapp/index.html');
